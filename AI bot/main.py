6c36d9469f6e499bdedc7625910fc6f608bedb34
helluvaFireLord <109943271+helluvaFireLord@users.noreply.github.com>
2023-10-28 21:22:17 +0700

    Add files via upload

---
+++ AI bot/main.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from model import get_class
+import os, random
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.command()
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f"./{attachment.filename}")
+    else:
+        await ctx.send("Вы забыли загрузить картинку :(")
+
+
+bot.run('')
